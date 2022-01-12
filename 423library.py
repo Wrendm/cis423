7496000a0fbc826060c7348ded64eccb3e7a4579
Wren Miles <72320514+Wrendm@users.noreply.github.com>
2022-01-12 13:19:18 -0800

    add ch2 transformers

--- 423library.py
+++ 423library.py
@@ -2,3 +2,75 @@ import pandas as pd
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
+
+
+class MappingTransformer(BaseEstimator, TransformerMixin):
+
+  def __init__(self, mapping_column, mapping_dict:dict):  
+    self.mapping_dict = mapping_dict
+    self.mapping_column = mapping_column  #column to focus on
+
+  def fit(self, X, y = None):
+    print("Warning: MappingTransformer.fit does nothing.")
+    return X
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'MappingTransformer.transform expected Dataframe but got {type(X)} instead.'
+    assert self.mapping_column in X.columns.to_list(), f'MappingTransformer.transform unknown column {self.mapping_column}'
+    X_ = X.copy()
+    X_[self.mapping_column].replace(self.mapping_dict, inplace=True)
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
+#############################################################################################################################
+class OHETransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, target_column, dummy_na=False, drop_first=True):  
+    self.target_column = target_column
+    self.dummy_na = dummy_na
+    self.drop_first = drop_first
+  #fill in the rest below
+  def fit(self, X, y = None):
+    print(f"Warning: {self.__class__.__name__}.fit does nothing.")
+    return self
+
+  def transform(self, X):
+    X = pd.get_dummies(X,
+                               prefix= self.target_column,    #your choice
+                               prefix_sep='_',     #your choice
+                               columns=[self.target_column],
+                               dummy_na= self.dummy_na,    #will try to impute later so leave NaNs in place
+                               drop_first= self.drop_first    #really should be True but I wanted to give a clearer picture
+                               )
+    return X
+
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
+  
+#############################################################################################################################
+class DropColumnsTransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, column_list, action='drop'):
+    assert action in ['keep', 'drop'], f'DropColumnsTransformer action {action} not in ["keep", "drop"]'
+    assert isinstance(column_list, list), f'DropColumnsTransformer expected list but saw {type(column_list)}'
+    self.column_list = column_list
+    self.action = action
+
+  #fill in rest below
+  def fit(self, X, y = None):
+    print(f"Warning: {self.__class__.__name__}.fit does nothing.")
+    return self
+
+  def transform(self, X):
+    X_ = X
+    if self.action == 'drop':
+      X_ = X_.drop(self.column_list, axis=1)
+    else:
+      X_ = X_.drop(X_.columns.difference(self.column_list), axis=1)
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
